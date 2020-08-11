92a03df69d1e103dd80961b5b6e460acb0a1a877
zmeriksen <zmeriksen@icloud.com>
2020-08-11 16:55:31 -0500

    basic poc

--- Sources/LaterCLI/main.swift
+++ Sources/LaterCLI/main.swift
@@ -1,1 +1,32 @@
-print("Hello, world!")
+import Foundation
+import ArgumentParser
+import Later
+
+struct LaterCLI: ParsableCommand {
+    @Argument()
+    var delay: UInt32
+    
+    @Argument()
+    var value: String
+    
+    mutating func run() throws {
+        let localValue = value
+        let sema = DispatchSemaphore(value: 0)
+        print("Waiting... \(delay) seconds")
+        Later.do(withDelay: delay) {
+            print("LaterCLI: \(localValue)")
+            sema.signal()
+        }
+        
+        var count = delay
+        Later.scheduleRepeatedTask(delay: .seconds(1)) { (task) in
+            print(count)
+            count -= 1
+        }
+        
+        sema.wait()
+    }
+}
+
+
+LaterCLI.main()
